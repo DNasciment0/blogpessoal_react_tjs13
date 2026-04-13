f78cb813a811badc256e659c7f04e2e1dc4db428
Daniel Nascimento <d.nasciimento@live.com>
2026-04-13 17:34:35 -0300

    ✨ New Feature: Adição dos componentes Navbar e Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <>
+      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+        <div className="container flex justify-between text-lg mx-8">
+          Blog do Daniel Nascimento
+          <div className="flex gap-4">
+            Postagem Temas Cadastrar Tema Perfil Sair
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
