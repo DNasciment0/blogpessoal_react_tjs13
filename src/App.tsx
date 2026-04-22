f80e7b1c0e32c55d4562c94a08e0d4d087cb7744
Daniel Nascimento <d.nasciimento@live.com>
2026-04-22 20:51:44 -0300

    ✨ New Feature:  Consumo do Recruso Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
-import Cadastro from "./pages/cadastro/Cadastro"
-import Login from "./pages/login/Login"
-import { AuthProvider } from "./contexts/AuthContext"
-import DeletarTema from "./components/tema/deletartema/DeletarTema"
-import FormTema from "./components/tema/formtema/FormTema"
-import ListaTemas from "./components/tema/listatemas/ListaTemas"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import FormTema from "./components/tema/formtema/FormTema";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
+import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
 
 function App() {
-  
   return (
     <>
       <AuthProvider>
@@ -24,16 +25,23 @@ function App() {
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
               <Route path="/temas" element={<ListaTemas />} />
-							<Route path="/cadastrartema" element={<FormTema />} />
-							<Route path="/editartema/:id" element={<FormTema />} />
-							<Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route
+                path="/deletarpostagem/:id"
+                element={<DeletarPostagem />}
+              />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
